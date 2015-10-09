7095e237836a19ac249d7d6e8d3c780b7a8755ac
NoHomey <ivo_stratev@abv.bg>
2015-10-09 21:10:20 +0300

    changing concpet of how url shoud change view

--- client/app.module.js
+++ client/app.module.js
@@ -1,10 +1,8 @@
 import { config, default as controllers } from '/config.js';
-import { run } from '/run.js';
 
 var moduleName = 'app';
 
 angular.module(moduleName, ['ngRoute', controllers.moduleName])
-  .config(config)
-  .run(run);
+  .config(config);
 
 export var app = moduleName;
--- client/config.js
+++ client/config.js
@@ -4,14 +4,17 @@ config.$inject = ['$routeProvider'];
 
 function config($routeProvider){
   $routeProvider
+    .when('/', root)
     .when(controllers.appController.url, controllers.appController.config)
     .when(controllers.loginController.url, controllers.loginController.config)
     .when(controllers.registerController.url, controllers.registerController.config)
-    .when(controllers.statsController.url, controllers.statsController.config);
+    .when(controllers.statsController.url, controllers.statsController.config)
+    .otherwise(root);
 }
 
-var url = [controllers.appController.url, controllers.loginController.url, controllers.registerController.url, controllers.statsController.url];
+var root = {
+  redirectTo : controllers.appController.url
+};
 
 export var config = config;
 export default { moduleName : controllers.moduleName };
-export var url = url;
