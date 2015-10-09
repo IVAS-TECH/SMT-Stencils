b4045f8e59401413c8682baa424dd41d4a7e0ed9
NoHomey <ivo_stratev@abv.bg>
2015-10-09 21:22:00 +0300

    .when('/', ...) is unnececery

--- client/config.js
+++ client/config.js
@@ -4,7 +4,6 @@ config.$inject = ['$routeProvider'];
 
 function config($routeProvider){
   $routeProvider
-    .when('/', root)
     .when(controllers.appController.url, controllers.appController.config)
     .when(controllers.loginController.url, controllers.loginController.config)
     .when(controllers.registerController.url, controllers.registerController.config)
