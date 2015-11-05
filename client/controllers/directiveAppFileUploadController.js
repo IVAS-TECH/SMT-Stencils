4a09e72234bea9785744005b81744d9b0148fdc2
NoHomey <ivo_stratev@abv.bg>
2015-11-05 20:23:42 +0200

    clent side file uploading is done

--- client/controllers/directiveAppFileUploadController.js
+++ client/controllers/directiveAppFileUploadController.js
@@ -1,13 +1,91 @@
 var controllerName = 'directiveAppFileUploadController';
 var directiveAppFileUploadController = {};
 
-Controller.$inject = ['Upload'];
+Controller.$inject = ['Upload', 'AppShowDialog', '$scope'];
 
-function Controller(Upload) {
+function Controller(Upload, AppShowDialog, $scope) {
   var vm = this;
   vm.files = [];
+  vm.fileUpload = fileUpload;
   vm.view = view;
   vm.removeFile = removeFile;
+
+  function fileUpload(files) {
+    if(files === null)
+      return;
+
+    var extention = 'Supported file formats are: .html';
+    var size = 'Maximum single file size allowed is 1GB';
+    var number = 'Maximum number of files allowed is 6';
+    var msg = '';
+    var newFiles = newFilesOnly(files);
+
+    if(newFiles.length === 0)
+      return;
+
+    if(!isValidNumber(newFiles))
+      msg += number;
+    if(!areValidExtenctions(newFiles))
+      msg += extention;
+    if(!areValidSizes(newFiles))
+      msg += size;
+
+    if(msg)
+      AppShowDialog(msg);
+    else
+      for(var i = 0; i < newFiles.length; ++i)
+        vm.files.push(newFiles[i]);
+
+    function newFilesOnly(files) {
+      var res = [];
+      files.forEach(isNew);
+      return res;
+
+      function isNew(file) {
+        var fileNames = vm.files.map(name);
+        var fileName = file.name;
+        if(!(_.includes(fileNames, fileName)))
+          res.push(file);
+
+        function name(item) {
+          var fName = item.name;
+          return fName;
+        }
+      }
+    }
+
+    function isValidNumber(files) {
+      var number = 6;
+      var valid = files.length < number;
+      return valid;
+    }
+
+    function areValidExtenctions(files) {
+      var valid = files.every(isValid);
+      return valid;
+
+      function isValid(item) {
+        var fileName = item.name;
+        var validExtention = ['html', 'js'];
+        var file = fileName.split('.');
+        var fileExtention = file[1];
+        var valid = _.includes(validExtention, fileExtention);
+        return valid;
+      }
+    }
+
+    function areValidSizes(files) {
+      var valid = files.every(isValid);
+      return valid;
+
+      function isValid(item) {
+        var fileSize = item.size;
+        var maxSize = 100000;
+        var valid = fileSize < maxSize;
+        return valid;
+      }
+    }
+  }
 
   function removeFile(i) {
     vm.files.splice(i, 1);
